66b15cb224547e8d4db925c282aab4879219a038
huynt77 <v.huynt77@vinsmartfuture.tech>
2025-12-15 10:37:34 +0700

    update config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,11 +6,18 @@ return {
         lazy = false,
         config = function()
             -- Install parsers
-            require('nvim-treesitter').install({ 'lua', 'vim', 'vimdoc', 'c', 'cpp', 'python', 'rust' })
+            require('nvim-treesitter').install({ 
+                'lua', 'vim', 'vimdoc', 'c', 'cpp', 'python', 'rust',
+                'javascript', 'typescript', 'tsx', 'css', 'html', 'json'
+            })
             
             -- Enable treesitter features via autocommand
             vim.api.nvim_create_autocmd('FileType', {
-                pattern = { 'lua', 'vim', 'c', 'cpp', 'python', 'rust' },
+                pattern = { 
+                    'lua', 'vim', 'c', 'cpp', 'python', 'rust',
+                    'javascript', 'typescript', 'typescriptreact', 'javascriptreact',
+                    'css', 'html', 'json'
+                },
                 callback = function()
                     -- Syntax highlighting (provided by Neovim)
                     vim.treesitter.start()
